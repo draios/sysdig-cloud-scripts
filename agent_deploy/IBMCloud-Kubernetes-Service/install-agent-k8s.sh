70d88df353a5819958171ede13d53eb868a39a88
Arsalan Rana <arsalan.rana@ca.ibm.com>
2018-10-18 15:01:23 -0400

    fixing access_key typo & enabling serviceAccount use

--- agent_deploy/IBMCloud-Kubernetes-Service/install-agent-k8s.sh
+++ agent_deploy/IBMCloud-Kubernetes-Service/install-agent-k8s.sh
@@ -95,7 +95,7 @@ function install_k8s_agent {
 
     echo "* Creating sysdig-agent secret using the ACCESS-KEY provided"
     fail=0
-    outsecret=$(kubectl create secret generic sysdig-agent --from-literal=access-key=$ACCESS-KEY 2>&1) || { fail=1 && echo "kubectl create serviceaccount failed!"; }
+    outsecret=$(kubectl create secret generic sysdig-agent --from-literal=access-key=$ACCESS_KEY 2>&1) || { fail=1 && echo "kubectl create serviceaccount failed!"; }
     if [ $fail -eq 1 ]; then
         if [[ "$outsecret" =~ "AlreadyExists" ]]; then
             echo "$outsecret. Continuing..."
@@ -141,6 +141,8 @@ function install_k8s_agent {
         echo "* Adding additional configuration to dragent.yaml"
         echo -e "    $ADDITIONAL_CONF" >> $CONFIG_FILE
     fi
+
+    sed -i "" "s|# serviceAccount: sysdig-agent|serviceAccount: sysdig-agent|" /tmp/sysdig-agent-daemonset-v2.yaml
 
     echo -e "    new_k8s: true" >> $CONFIG_FILE
     kubectl apply -f $CONFIG_FILE
