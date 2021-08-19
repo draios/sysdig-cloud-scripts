3fd952a63459750e92e128b21f35a900b2bf0918
Julio J <papajulio21@gmail.com>
2021-08-19 17:24:12 +0200

    Initial version of the changes after manual review

--- agent_deploy/IBMCloud-Kubernetes-Service/install-agent-k8s.sh
+++ agent_deploy/IBMCloud-Kubernetes-Service/install-agent-k8s.sh
@@ -32,20 +32,33 @@ function install_curl_rpm {
 }
 
 function download_yamls {
-    echo "* Downloading Sysdig cluster role yaml"
-    curl -s -o /tmp/sysdig-agent-clusterrole.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-agent-clusterrole.yaml
-    echo "* Downloading Sysdig config map yaml"
-    curl -s -o /tmp/sysdig-agent-configmap.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-agent-configmap.yaml
-    echo "* Downloading Sysdig daemonset v2 yaml"
-    curl -s -o /tmp/sysdig-agent-daemonset-v2.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-agent-daemonset-v2.yaml
+	WORKDIR=$(mktemp -d /tmp/sysdig-agent-k8s.XXXXXX)
+	echo "* Downloading yamls files to the temp directory: $WORKDIR"
+	echo "* Downloading Sysdig cluster role yaml"
+	curl -s -o $WORKDIR/sysdig-agent-clusterrole.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-agent-clusterrole.yaml
+	echo "* Downloading Sysdig config map yaml"
+	curl -s -o $WORKDIR/sysdig-agent-configmap.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-agent-configmap.yaml
+	echo "* Downloading Sysdig daemonset v2 yaml"
+	curl -s -o $WORKDIR/sysdig-agent-daemonset-v2.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-agent-daemonset-v2.yaml
+	echo "* Downloading Sysdig daemonset slim v2 yaml"
+	curl -s -o $WORKDIR/sysdig-agent-slim-daemonset-v2.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-agent-slim-daemonset-v2.yaml
     echo "* Downloading Sysdig kmod-thin-agent-slim daemonset"
-    curl -s -o /tmp/sysdig-kmod-thin-agent-slim-daemonset.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-kmod-thin-agent-slim-daemonset.yaml
-    if [ $INSTALL_ANALYZER -eq 1 ]; then
-      echo "* Downloading Sysdig Image Analyzer config map yaml"
-      curl -H 'Cache-Control: no-cache' -s -o /tmp/sysdig-image-analyzer-configmap.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-image-analyzer-configmap.yaml
-      echo "* Downloading Sysdig Image Analyzer daemonset v1 yaml"
-      curl -H 'Cache-Control: no-cache' -s -o /tmp/sysdig-image-analyzer-daemonset.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-image-analyzer-daemonset.yaml
-    fi
+    curl -s -o $WORKDIR/sysdig-kmod-thin-agent-slim-daemonset.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-kmod-thin-agent-slim-daemonset.yaml
+	if [ $INSTALL_IMAGE_ANALYZER -eq 1 ]; then
+		echo "* Downloading Sysdig Image Analyzer config map yaml"
+		curl -H 'Cache-Control: no-cache' -s -o $WORKDIR/sysdig-image-analyzer-configmap.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-image-analyzer-configmap.yaml
+		echo "* Downloading Sysdig Image Analyzer daemonset v1 yaml"
+		curl -H 'Cache-Control: no-cache' -s -o $WORKDIR/sysdig-image-analyzer-daemonset.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-image-analyzer-daemonset.yaml
+	elif [ $INSTALL_NODE_ANALYZER -eq 1 ]; then
+		echo "* Downloading Sysdig Image Analyzer config map yaml"
+		curl -H 'Cache-Control: no-cache' -s -o $WORKDIR/sysdig-image-analyzer-configmap.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-image-analyzer-configmap.yaml
+		echo "* Downloading Sysdig Benchmark Runner config map yaml"
+		curl -H 'Cache-Control: no-cache' -s -o $WORKDIR/sysdig-benchmark-runner-configmap.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-benchmark-runner-configmap.yaml
+		echo "* Downloading Sysdig Host Analyzer config map yaml"
+		curl -H 'Cache-Control: no-cache' -s -o $WORKDIR/sysdig-host-analyzer-configmap.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-host-analyzer-configmap.yaml
+		echo "* Downloading Sysdig Node Analyzer daemonset v1 yaml"
+		curl -H 'Cache-Control: no-cache' -s -o $WORKDIR/sysdig-node-analyzer-daemonset.yaml https://raw.githubusercontent.com/draios/sysdig-cloud-scripts/master/agent_deploy/kubernetes/sysdig-node-analyzer-daemonset.yaml
+	fi
 }
 
 function unsupported {
@@ -63,6 +76,7 @@ function help {
     echo "                [-cp | --collector_port <value>] [-s | --secure <value>] [-cc | --check_certificate <value>] \ "
     echo "                [-ns | --namespace | --project <value>] [-ac | --additional_conf <value>] [-np | --no-prometheus] \ "
     echo "                [-sn | --sysdig_instance_name <value>] [-op | --openshift] [-af | --agent-full] [-as | --agent-slim] \ "
+	echo "                [-ae | --api_endpoint <value> ] [-na | --nodeanalyzer ] \ "
     echo "                [-ia | --imageanalyzer ] [-am | --analysismanager <value>] [-ds | --dockersocket <value>] [-cs | --crisocket <value>] [-cv | --customvolume <value>] \ "
     echo "                [-av | --agent-version <value>] [ -r | --remove ] [ -aws | --aws ] [-h | --help]"
     echo ""
@@ -77,11 +91,16 @@ function help {
     echo " -np : if provided, do not enable the Prometheus collector.  Defaults to enabling Prometheus collector"
     echo " -sn : if provided, name of the sysdig instance (optional)"
     echo " -op : if provided, perform the installation using the OpenShift command line"
+
     echo " -as : if provided, use agent-slim (this is the default agent). Note: this option is not required"
     echo " -af : if provided, use agent-full instead of agent-slim"
+	echo " -ac : if provided, the additional configuration will be appended to agent configuration file"
+	echo " -av : if provided, use the agent-version specified. (default: latest)"
     echo " -r  : if provided, will remove the sysdig agent's daemonset, configmap, clusterrolebinding,"
     echo "       serviceacccount and secret from the specified namespace"
-    echo " -ia : if provided, will install the Node Image Analyzer"
+	echo " -ae : if provided, will be used as the base (host) for the Node Analyzer endpoints."
+	echo " -na : if provided, will install the Node Analyzer. It is an error to set both -ia and -na."
+	echo " -ia : if provided, will install the Node Image Analyzer. It is an error to set both -ia and -na."
     echo " -aws : if provided, will support AWS cluster name parsing and not use ICR"
     echo " -am : Analysis Manager endpoint for Sysdig Secure"
     echo " -ds : docker socket for Image Analyzer"
@@ -108,7 +127,10 @@ function create_namespace {
         out=$(kubectl create namespace $NAMESPACE 2>&1) || { fail=1 && echo "kubectl create namespace failed!"; }
     else
         echo "* Creating project: $NAMESPACE"
-        out=$(oc adm new-project $NAMESPACE --node-selector='' 2>&1) || { fail=1 && echo "oc adm new-project failed!"; }
+		out=$(oc adm new-project $NAMESPACE --node-selector='app=sysdig-agent' 2>&1) || { fail=1 && echo "oc adm new-project failed!"; }
+		# label all nodes
+		oc label node --all "app=sysdig-agent"
+
         # Set the project to the namespace
         switch=$(oc project $NAMESPACE 2>&1)
     fi
@@ -143,15 +165,14 @@ function create_sysdig_serviceaccount {
 }
 
 function install_k8s_agent {
+    fail=0
     if [ $OPENSHIFT -eq 0 ]; then
         echo "* Creating sysdig-agent clusterrole and binding"
-        kubectl apply -f /tmp/sysdig-agent-clusterrole.yaml
-        fail=0
+        kubectl apply -f $WORKDIR/sysdig-agent-clusterrole.yaml
         outbinding=$(kubectl create clusterrolebinding sysdig-agent --clusterrole=sysdig-agent --serviceaccount=$NAMESPACE:sysdig-agent --namespace=$NAMESPACE 2>&1) || \
           { fail=1 && echo "kubectl create clusterrolebinding failed!"; }
     else
         echo "* Creating sysdig-agent access policies"
-        fail=0
         outbinding=$(oc adm policy add-scc-to-user privileged -n $NAMESPACE -z sysdig-agent 2>&1) || { fail=1 && echo "oc adm policy add-scc-to-user failed!"; }
         if [ $fail -eq 0 ]; then
             outbinding=$(oc adm policy add-cluster-role-to-user cluster-reader -n $NAMESPACE -z sysdig-agent 2>&1) || { fail=1 && echo "oc adm policy add-cluster-role-to-user failed!"; }
@@ -180,7 +201,7 @@ function install_k8s_agent {
         fi
     fi
 
-    CONFIG_FILE=/tmp/sysdig-agent-configmap.yaml
+    CONFIG_FILE=$WORKDIR/sysdig-agent-configmap.yaml
 
     echo "* Retrieving the Cluster ID and Cluster Name"
     IKS_CLUSTER_ID=$(kubectl get cm -n kube-system cluster-info -o yaml | grep ' "cluster_id": ' | cut -d'"' -f4)
@@ -263,51 +284,94 @@ function install_k8s_agent {
         echo -e "        enabled: true" >> $CONFIG_FILE
     fi
 
-    if [ $INSTALL_ANALYZER -eq 1 ]; then
-      # Image Analyzer config map
-      IA_CONFIG_FILE=/tmp/sysdig-image-analyzer-configmap.yaml
+    echo -e "    new_k8s: true" >> $CONFIG_FILE
+    kubectl apply -f $CONFIG_FILE --namespace=$NAMESPACE
 
-      # If the collector was changed but the analysis manager was not this was
-      # most likely an onprem install, add the default analysis manager for that onprem
-      if [ ! -z "$COLLECTOR" ] && [ -z "$ANALYSIS_MANAGER" ]; then
-        ANALYSIS_MANAGER="https://${COLLECTOR}/internal/scanning/scanning-analysis-collector"
-        echo "* Configuring Analysis Manager endpoint to ${ANALYSIS_MANAGER}. You can also use the -am option to explicitly specify it."
-      fi
+	if [ $INSTALL_IMAGE_ANALYZER -eq 1 ] || [ $INSTALL_NODE_ANALYZER -eq 1 ]; then
+        # Image Analyzer config map
+        IA_CONFIG_FILE=$WORKDIR/sysdig-image-analyzer-configmap.yaml
 
-      if [ ! -z "$ANALYSIS_MANAGER" ]; then
-        echo "* Setting Analysis Manager endpoint for Image Analyzer"
-        echo "  collector_endpoint: $ANALYSIS_MANAGER" >> $IA_CONFIG_FILE
-      fi
-      if [ ! -z "$DOCKER_SOCKET_PATH" ]; then
-        echo "* Setting docker socket path"
-        echo "  docker_socket_path: $DOCKER_SOCKET_PATH" >> $IA_CONFIG_FILE
-      fi
-      if [ ! -z "$CRI_SOCKET_PATH" ]; then
-        echo "* Setting CRI socket path"
-        echo "  cri_socket_path: $CRI_SOCKET_PATH" >> $IA_CONFIG_FILE
-      fi
-      if [ ! -z "$CRI_CONTAINERD_SOCKET_PATH" ]; then
-        echo "* Setting CRI-containerd socket path"
-        echo "  containerd_socket_path: $CRI_CONTAINERD_SOCKET_PATH" >> $IA_CONFIG_FILE
-      fi
-      if [ ! -z "$CHECK_CERT" ]; then
-        echo "* Setting SSL certificate check level"
-        echo "  ssl_verify_certificate: \"$CHECK_CERT\"" >> $IA_CONFIG_FILE
-      else
-        echo "  ssl_verify_certificate: \"true\"" >> $IA_CONFIG_FILE
-      fi
+		# If the collector was changed but the analysis manager was not this was
+		# most likely an onprem install, add the default analysis manager for that onprem
+		if [ ! -z "$API_ENDPOINT" ] && [ -z "$ANALYSIS_MANAGER" ]; then
+			ANALYSIS_MANAGER="https://${API_ENDPOINT}/internal/scanning/scanning-analysis-collector"
+			echo "* Configuring Analysis Manager endpoint to ${ANALYSIS_MANAGER}. You can also use the -am option to explicitly specify it."
+		elif [ ! -z "$COLLECTOR" ] && [ -z "$ANALYSIS_MANAGER" ]; then
+			ANALYSIS_MANAGER="https://${COLLECTOR}/internal/scanning/scanning-analysis-collector"
+			echo "* Configuring Analysis Manager endpoint to ${ANALYSIS_MANAGER}. You can also use the -am option to explicitly specify it."
+		fi
 
-      kubectl apply -f $IA_CONFIG_FILE --namespace=$NAMESPACE
+        if [ ! -z "$ANALYSIS_MANAGER" ]; then
+          echo "* Setting Analysis Manager endpoint for Image Analyzer"
+          echo "  collector_endpoint: $ANALYSIS_MANAGER" >> $IA_CONFIG_FILE
+        fi
+        if [ ! -z "$DOCKER_SOCKET_PATH" ]; then
+          echo "* Setting docker socket path"
+          echo "  docker_socket_path: $DOCKER_SOCKET_PATH" >> $IA_CONFIG_FILE
+        fi
+        if [ ! -z "$CRI_SOCKET_PATH" ]; then
+          echo "* Setting CRI socket path"
+          echo "  cri_socket_path: $CRI_SOCKET_PATH" >> $IA_CONFIG_FILE
+        fi
+        if [ ! -z "$CRI_CONTAINERD_SOCKET_PATH" ]; then
+          echo "* Setting CRI-containerd socket path"
+          echo "  containerd_socket_path: $CRI_CONTAINERD_SOCKET_PATH" >> $IA_CONFIG_FILE
+        fi
+        if [ ! -z "$CHECK_CERT" ]; then
+          echo "* Setting SSL certificate check level"
+          echo "  ssl_verify_certificate: \"$CHECK_CERT\"" >> $IA_CONFIG_FILE
+        else
+          echo "  ssl_verify_certificate: \"true\"" >> $IA_CONFIG_FILE
+        fi
+
+        kubectl apply -f $IA_CONFIG_FILE --namespace=$NAMESPACE
     fi
+
+	if [ $INSTALL_NODE_ANALYZER -eq 1 ]; then
+		# Benchmark Runner config map
+		BR_CONFIG_FILE=$WORKDIR/sysdig-benchmark-runner-configmap.yaml
+
+		if [ ! -z "$API_ENDPOINT" ]; then
+			echo "* Setting API endpoint for Benchmark Runner"
+			echo "  collector_endpoint: https://$API_ENDPOINT" >> $BR_CONFIG_FILE
+		elif [ ! -z "$COLLECTOR" ]; then
+			echo "* Setting Collector endpoint for Benchmark Runner"
+			echo "  collector_endpoint: https://$COLLECTOR" >> $BR_CONFIG_FILE
+		fi
+
+		if [ ! -z "$CHECK_CERT" ]; then
+			echo "* Setting SSL certificate check level"
+			echo "  ssl_verify_certificate: \"$CHECK_CERT\"" >> $BR_CONFIG_FILE
+		else
+			echo "  ssl_verify_certificate: \"true\"" >> $BR_CONFIG_FILE
+		fi
+
+		kubectl apply -f $BR_CONFIG_FILE --namespace=$NAMESPACE
+
+		# Host Analyzer config map
+		HA_CONFIG_FILE=$WORKDIR/sysdig-host-analyzer-configmap.yaml
+
+		echo "* Using Analysis Manager endpoint for Host Analyzer: ${ANALYSIS_MANAGER}"
+		echo "  collector_endpoint: $ANALYSIS_MANAGER" >> $HA_CONFIG_FILE
+
+		if [ ! -z "$CHECK_CERT" ]; then
+			echo "* Setting SSL certificate check level"
+			echo "  ssl_verify_certificate: \"$CHECK_CERT\"" >> $HA_CONFIG_FILE
+		else
+			echo "  ssl_verify_certificate: \"true\"" >> $HA_CONFIG_FILE
+		fi
+
+		kubectl apply -f $HA_CONFIG_FILE --namespace=$NAMESPACE
+	fi
 
     if [ $AGENT_FULL -eq 1 ]; then
         echo "Full agent selected "
-        DAEMONSET_FILE="/tmp/sysdig-agent-daemonset-v2.yaml"
+        DAEMONSET_FILE="$WORKDIR/sysdig-agent-daemonset-v2.yaml"
         AGENT_STRING="agent"
         AGENT_NAMES="agent"
     else
         echo "Slim agent selected "
-        DAEMONSET_FILE="/tmp/sysdig-kmod-thin-agent-slim-daemonset.yaml"
+        DAEMONSET_FILE="$WORKDIR/sysdig-kmod-thin-agent-slim-daemonset.yaml"
         AGENT_STRING="agent-slim"
         AGENT_NAMES="agent-slim agent-kmodule"
     fi
@@ -370,8 +434,6 @@ function install_k8s_agent {
     fi
     rm -f $DAEMONSET_FILE.bak
 
-    echo -e "    new_k8s: true" >> $CONFIG_FILE
-    kubectl apply -f $CONFIG_FILE --namespace=$NAMESPACE
 
     echo "* Deploying the sysdig agent"
     kubectl apply -f $DAEMONSET_FILE --namespace=$NAMESPACE
@@ -383,9 +445,9 @@ function install_k8s_agent {
     echo "Should any pod not reach the \"Running\" state, further info can be obtained from logs as follows"
     echo "'kubectl logs <agent-pod-name> -n $NAMESPACE' "
 
-    if [ $INSTALL_ANALYZER -eq 1 ]; then
+    if [ $INSTALL_IMAGE_ANALYZER -eq 1 ]; then
       # Deploy Image Analyzer
-      IA_FILE=/tmp/sysdig-image-analyzer-daemonset.yaml
+      IA_FILE=$WORKDIR/sysdig-image-analyzer-daemonset.yaml
       if [ ! -z "$IA_CUSTOM_PATH" ]; then
         NL="\n"
         if [[ $uname -eq "Darwin" ]]; then
@@ -408,6 +470,31 @@ function install_k8s_agent {
 
       echo -e "\nThe list of Image Analyzers pods deployed in the namespace \"$NAMESPACE\" are:"
       kubectl get pods -n $NAMESPACE | grep "image-analyzer"
+	elif [ $INSTALL_NODE_ANALYZER -eq 1 ]; then
+		# Deploy Image Analyzer
+		NA_FILE=$WORKDIR/sysdig-node-analyzer-daemonset.yaml
+		if [ ! -z "$NA_CUSTOM_PATH" ]; then
+			NL="\n"
+			if [[ $uname -eq "Darwin" ]]; then
+				NL=$'\\\n'
+			fi
+
+			IA_MATCH="Add custom volume here"
+			IA_INSERT_VOLUME="      - name: custom-volume${NL}        hostPath:${NL}          path: ${IA_CUSTOM_PATH}"
+			
+			sed -i.bak -e "s|${IA_MATCH}|${IA_MATCH}${NL}${IA_INSERT_VOLUME}|" $NA_FILE
+
+			IA_MATCH="Add custom volume mount here"
+			IA_INSERT_VOLUME="        - mountPath: ${IA_CUSTOM_PATH}${NL}          name: custom-volume"
+
+			sed -i.bak -e "s|${IA_MATCH}|${IA_MATCH}${NL}${IA_INSERT_VOLUME}|" $NA_FILE
+		fi
+
+		echo "* Deploying the Node Analyzer"
+		kubectl apply -f $NA_FILE --namespace=$NAMESPACE
+
+		echo -e "\nThe list of Node Analyzer pods deployed in the namespace \"$NAMESPACE\" are:"
+		kubectl get pods -n $NAMESPACE | grep "node-analyzer"
     fi
 }
 
@@ -431,19 +518,36 @@ function remove_agent {
 
       echo "* Deleting the sysdig-image-analyzer configmap"
       kubectl delete configmap sysdig-image-analyzer --namespace=$NAMESPACE
+	elif [ "$(kubectl get pods -n $NAMESPACE | grep -c "node-analyzer")" -ge 1 ]; then
+		echo "* Deleting the sysdig-node-analyzer daemonset"
+		kubectl delete daemonset sysdig-node-analyzer --namespace=$NAMESPACE
+
+		echo "* Deleting the sysdig-image-analyzer configmap"
+		kubectl delete configmap sysdig-image-analyzer --namespace=$NAMESPACE
+
+		echo "* Deleting the sysdig-benchmark-runner configmap"
+		kubectl delete configmap sysdig-benchmark-runner --namespace=$NAMESPACE
     fi
 
     if [ $OPENSHIFT -eq 0 ]; then
         echo "* deleting the sysdig-agent clusterrolebinding"
         kubectl delete clusterrolebinding sysdig-agent --namespace=$NAMESPACE
+
+		echo "* Deleting the sysdig-agent clusterrole"
+		kubectl delete clusterrole sysdig-agent
     else
         echo "* Removing cluster role and security constraints"
         oc adm policy remove-cluster-role-from-user cluster-reader -n $NAMESPACE -z sysdig-agent
         oc adm policy remove-scc-from-user privileged -n $NAMESPACE -z sysdig-agent
+
+		echo "* Deleting labels from oc nodes"
+		oc label node --all app-
     fi
 
     echo "* Deleting the sysdig-agent secret"
     kubectl delete secret sysdig-agent --namespace=$NAMESPACE
+
+	echo "* Note, the namespace '$NAMESPACE' is not deleted. It could have other resources"
 
     set -e
 }
@@ -460,10 +564,12 @@ NAMESPACE="ibm-observe"
 REMOVE_AGENT=0
 ENABLE_PROMETHEUS=1
 OPENSHIFT=0
-INSTALL_ANALYZER=0
+INSTALL_IMAGE_ANALYZER=0
+INSTALL_NODE_ANALYZER=0
 AGENT_VERSION="latest"
 AWS=0
 AGENT_FULL=0
+WORKDIR=""
 
 while [[ ${#} > 0 ]]
 do
@@ -624,8 +730,20 @@ case ${key} in
         fi
         shift
         ;;
+    -ae|--api_endpoint)
+        if is_valid_value "${2}"; then
+            API_ENDPOINT="${2}"
+        else
+            echo "ERROR: no value provided for API endpoint option, use -h | --help for $(basename ${0}) Usage"
+            exit 1
+        fi
+        shift
+        ;;
     -ia|--imageanalyzer)
-        INSTALL_ANALYZER=1
+        INSTALL_IMAGE_ANALYZER=1
+        ;;
+    -na|--nodeanalyzer)
+        INSTALL_NODE_ANALYZER=1
         ;;
     -h|--help)
         help
