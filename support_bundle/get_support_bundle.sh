3f5ea3a4fd78b3ff9490c4dc339939348ed70996
Drew Thomson <drew.thomson@sysdig.com>
2022-02-28 08:56:10 -0600

    update support bundle

--- support_bundle/get_support_bundle.sh
+++ support_bundle/get_support_bundle.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
+trap 'catch' ERR
+catch() {
+  echo "An error has occurred. Please check your input and try again. Run this script with the -d flag for debugging"
+}
+
+gnudate() {
+    if hash gdate 2>/dev/null; then
+        gdate "$@"
+    else
+        date "$@"
+    fi
+}
+
 #generate sysdigcloud support bundle on kubernetes
 
 LABELS=""
@@ -8,8 +21,12 @@ CONTEXT=""
 CONTEXT_OPTS=""
 NAMESPACE=""
 LOG_DIR=$(mktemp -d sysdigcloud-support-bundle-XXXX)
+SINCE_OPTS="" 
+SINCE=""
+API_KEY=""
+ELASTIC_CURL=""
 
-while getopts l:n:c:hced flag; do
+while getopts l:n:c:s:a:hced flag; do
     case "${flag}" in
         l) LABELS=${OPTARG:-};;
         n) NAMESPACE=${OPTARG:-};;
@@ -18,22 +35,34 @@ while getopts l:n:c:hced flag; do
             echo "Usage: ./get_support_bundle.sh -n <NAMESPACE> -l <LABELS>"; 
             echo "Example: ./get_support_bundle.sh -n sysdig -l api,collector,worker,cassandra,elasticsearch"; 
             echo "Flags:"; 
+            echo "-a  Provide the Superuser API key for advanced data collection"
+            echo "-c  Specify the kubectl context. If not set, the current context will be used.";
+            echo "-d  Run the script in debug mode (use this if you encounter a problem).";
             echo "-n  Specify the Sysdig namespace. If not specified, "sysdigcloud" is assumed."; 
-            echo "-c  Specify the kubectl context. If not set, the current context will be used."; 
             echo "-l  Specify Sysdig pod role label to collect (e.g. api,collector,worker)";
             echo "-h  Print these instructions";
+            echo "-s  Specify the timeframe of logs to collect (e.g. -s 1h)"
             exit;;
 
         c) CONTEXT=${OPTARG:-};;
+        s) SINCE=${OPTARG:-};;
+        a) API_KEY=${OPTARG:-};;
+        d) set -x;;
+
     esac
 done
 
+# Check for supplied namespace, kube context, and flags
 if [[ -z ${NAMESPACE} ]]; then
     NAMESPACE="sysdig"
 fi
 
 if [[ ! -z ${CONTEXT} ]]; then
     CONTEXT_OPTS="--context=${CONTEXT}"
+fi
+
+if [[ ! -z ${SINCE} ]]; then
+    SINCE_OPTS="--since ${SINCE}"
 fi
 
 # Set options for kubectl commands
@@ -47,6 +76,53 @@ if [[ "$(echo "$KUBE_OUTPUT" | grep -o "^sysdig " || true)" != "${NAMESPACE} " ]
     echo "We could not determine the namespace. Please check the spelling and try again";
     echo "kubectl ${KUBE_OPTS} get ns";
     echo "$(kubectl ${KUBE_OPTS} get ns)";
+fi
+
+get_metrics() {
+# function used to get metric JSON data for particular metrics we are interested in from the agent
+# arguments:
+# 1 - metric_name
+# 2 - segment_by
+metric="${1}"
+segment_by="${2}"
+
+        PARAMS=(
+          -sk --location --request POST "${API_URL}/api/data/batch?metricCompatibilityValidation=true&emptyValuesAsNull=true"
+          --header 'X-Sysdig-Product: SDC'
+          --header "Authorization: Bearer ${API_KEY}"
+          --header 'Content-Type: application/json'
+          --data-raw "{\"requests\":[{\"format\":{\"type\":\"data\"},\"time\":{\"from\":${FROM_EPOCH_TIME}000000,\"to\":${TO_EPOCH_TIME}000000,\"sampling\":600000000},\"metrics\":{\"v0\":\"${metric}\",\"k0\":\"timestamp\",\"k1\":\"${segment_by}\"},\"group\":{\"aggregations\":{\"v0\":\"avg\"},\"groupAggregations\":{\"v0\":\"avg\"},\"by\":[{\"metric\":\"k0\",\"value\":600000000},{\"metric\":\"k1\"}],\"configuration\":{\"groups\":[{\"groupBy\":[]}]}},\"paging\":{\"from\":0,\"to\":9},\"sort\":[{\"v0\":\"desc\"}],\"scope\":null,\"compareTo\":null}]}'"
+        )
+        curl "${PARAMS[@]}" >${LOG_DIR}/metrics/${metric}_${segment_by}.json
+}
+
+# If API key is supplied, collect streamSnap, Index settings, and fastPath settings
+if [[ ! -z ${API_KEY} ]]; then
+    API_URL=$(kubectl ${KUBE_OPTS} get cm sysdigcloud-config -o yaml | grep -i api.url: | head -1 | awk '{print$2}')
+    curl -ks -H "Authorization: Bearer ${API_KEY}" -H "Content-Type: application/json" "${API_URL}/api/admin/customer/1/streamsnapSettings" >> ${LOG_DIR}/streamSnap_settings.json
+    curl -ks -H "Authorization: Bearer ${API_KEY}" -H "Content-Type: application/json" "${API_URL}/api/admin/customer/1/fastPathSettings" >> ${LOG_DIR}/fastPath_settings.json
+    curl -ks -H "Authorization: Bearer ${API_KEY}" -H "Content-Type: application/json" "${API_URL}/api/admin/customer/1/indexSettings" >> ${LOG_DIR}/index_settings.json
+    curl -ks -H "Authorization: Bearer ${API_KEY}" -H "Content-Type: application/json" "${API_URL}/api/admin/customer/1/planSettings" >> ${LOG_DIR}/plan_settings.json
+    curl -ks -H "Authorization: Bearer ${API_KEY}" -H "Content-Type: application/json" "${API_URL}/api/admin/customer/1/dataRetentionSettings" >> ${LOG_DIR}/dataRetention_settings.json
+    curl -ks -H "Authorization: Bearer ${API_KEY}" -H "Content-Type: application/json" "${API_URL}/api/agents/connected" >> ${LOG_DIR}/agents-connected.json
+
+    TO_EPOCH_TIME=$(gnudate -d "$(gnudate +%H):00:00" +%s)
+    FROM_EPOCH_TIME=$((TO_EPOCH_TIME-86400))
+    METRICS=("syscall.count" "dragent.analyzer.sr" "container.count" "dragent.analyzer.n_drops_buffer" "dragent.analyzer.n_evts")
+    DEFAULT_SEGMENT="host.hostName"
+    SYSCALL_SEGMENTS=("host.hostName" "proc.name")
+
+    mkdir -p ${LOG_DIR}/metrics
+    for metric in ${METRICS[@]}; do
+        if [ "${metric}" == "syscall.count" ]; then
+            for segment in ${SYSCALL_SEGMENTS[@]}; do
+                get_metrics "${metric}" "${segment}"
+            done
+        else
+            get_metrics "${metric}" "${DEFAULT_SEGMENT}"
+        fi
+    done
+
 fi
 
 # Configure kubectl command if labels are set
@@ -69,8 +145,17 @@ do
     containers=$(kubectl ${KUBE_OPTS} get pod ${pod} -o json | jq -r '.spec.containers[].name')
     for container in ${containers}; 
     do
-        kubectl ${KUBE_OPTS} logs ${pod} -c ${container} > ${LOG_DIR}/${pod}/${container}-kubectl-logs.txt
-        kubectl ${KUBE_OPTS} exec ${pod} -c ${container} -- bash -c "${command}" > ${LOG_DIR}/${pod}/${container}-support-files.tgz || true
+        kubectl ${KUBE_OPTS} logs ${pod} -c ${container} ${SINCE_OPTS} > ${LOG_DIR}/${pod}/${container}-kubectl-logs.txt
+        echo "Execing into ${container}"
+        kubectl ${KUBE_OPTS} exec ${pod} -c ${container} -- bash >/dev/null 2>&1 && RETVAL=$? || RETVAL=$? && true
+        kubectl ${KUBE_OPTS} exec ${pod} -c ${container} -- sh >/dev/null 2>&1 && RETVAL1=$? || RETVAL1=$? && true
+        if [ $RETVAL -eq 0 ]; then
+            kubectl ${KUBE_OPTS} exec ${pod} -c ${container} -- bash -c "${command}" > ${LOG_DIR}/${pod}/${container}-support-files.tgz || true
+        elif [ $RETVAL1 -eq 0 ]; then
+            kubectl ${KUBE_OPTS} exec ${pod} -c ${container} -- sh -c "${command}" > ${LOG_DIR}/${pod}/${container}-support-files.tgz || true
+        else
+            echo "Skipping log gathering for ${pod}"
+        fi
     done
 done
 
@@ -112,36 +197,89 @@ printf "Containers: ${num_total_containers}\n" >> ${LOG_DIR}/container_density.t
 
 # Fetch Cassandra Nodetool output
 echo "Fetching Cassandra statistics";
-mkdir -p ${LOG_DIR}/cassandra
 for pod in $(kubectl ${KUBE_OPTS} get pod -l role=cassandra | grep -v "NAME" | awk '{print $1}')
 do
-    printf "$pod\t" |tee -a ${LOG_DIR}/cassandra/nodetool_info.log
-    kubectl ${KUBE_OPTS} exec -it $pod -c cassandra -- nodetool info >> ${LOG_DIR}/cassandra/nodetool_info.log
-    kubectl ${KUBE_OPTS} exec -it $pod -c cassandra -- nodetool status | tee -a ${LOG_DIR}/cassandra/nodetool_status.log
-    kubectl ${KUBE_OPTS} exec -it $pod -c cassandra -- nodetool getcompactionthroughput | tee -a ${LOG_DIR}/cassandra/nodetool_getcompactionthroughput.log
-    kubectl ${KUBE_OPTS} exec -it $pod -c cassandra -- nodetool cfstats | tee -a ${LOG_DIR}/cassandra/nodetool_cfstats.log
-    kubectl ${KUBE_OPTS} exec -it $pod -c cassandra -- nodetool cfhistograms draios message_data10 | tee -a ${LOG_DIR}/cassandra/nodetool_cfhistograms.log
-    kubectl ${KUBE_OPTS} exec -it $pod -c cassandra -- nodetool proxyhistograms | tee -a ${LOG_DIR}/cassandra/nodetool_proxyhistograms.log
-    kubectl ${KUBE_OPTS} exec -it $pod -c cassandra -- nodetool tpstats | tee -a ${LOG_DIR}/cassandra/nodetool_tpstats.log
-    kubectl ${KUBE_OPTS} exec -it $pod -c cassandra -- nodetool compactionstats | tee -a ${LOG_DIR}/cassandra/nodetool_compactionstats.log
+    mkdir -p ${LOG_DIR}/cassandra/${pod}
+    kubectl ${KUBE_OPTS} exec -it ${pod} -c cassandra -- nodetool info | tee -a ${LOG_DIR}/cassandra/${pod}/nodetool_info.log
+    kubectl ${KUBE_OPTS} exec -it ${pod} -c cassandra -- nodetool status | tee -a ${LOG_DIR}/cassandra/${pod}/nodetool_status.log
+    kubectl ${KUBE_OPTS} exec -it ${pod} -c cassandra -- nodetool getcompactionthroughput | tee -a ${LOG_DIR}/cassandra/${pod}/nodetool_getcompactionthroughput.log
+    kubectl ${KUBE_OPTS} exec -it ${pod} -c cassandra -- nodetool cfstats | tee -a ${LOG_DIR}/cassandra/${pod}/nodetool_cfstats.log
+    kubectl ${KUBE_OPTS} exec -it ${pod} -c cassandra -- nodetool cfhistograms draios message_data10 | tee -a ${LOG_DIR}/cassandra/${pod}/nodetool_cfhistograms.log
+    kubectl ${KUBE_OPTS} exec -it ${pod} -c cassandra -- nodetool proxyhistograms | tee -a ${LOG_DIR}/cassandra/${pod}/nodetool_proxyhistograms.log
+    kubectl ${KUBE_OPTS} exec -it ${pod} -c cassandra -- nodetool tpstats | tee -a ${LOG_DIR}/cassandra/${pod}/nodetool_tpstats.log
+    kubectl ${KUBE_OPTS} exec -it ${pod} -c cassandra -- nodetool compactionstats | tee -a ${LOG_DIR}/cassandra/${pod}/nodetool_compactionstats.log
 done
 
-# Fetch Elasticsearch storage info
+echo "Fetching Elasticsearch health info"
 mkdir -p ${LOG_DIR}/elasticsearch
-printf "Pod#\tFilesystem\tSize\tUsed\tAvail\tUse\tMounted on\n" |tee -a ${LOG_DIR}/elasticsearch/elasticsearch_storage.log
+# CHECK HERE IF THE TLS ENV VARIABLE IS SET IN ELASTICSEARCH, AND BUILD THE CURL COMMAND OUT
+ELASTIC_POD=$(kubectl ${KUBE_OPTS} get po -l role=elasticsearch --no-headers | head -1 | awk '{print $1}')
+ELASTIC_TLS=$(kubectl ${KUBE_OPTS} exec -it ${ELASTIC_POD} -- env | grep -i ELASTICSEARCH_TLS_ENCRYPTION)
+
+if [[ ${ELASTIC_TLS} == *"ELASTICSEARCH_TLS_ENCRYPTION=true"* ]]; then
+    ELASTIC_CURL='curl -s --cacert /usr/share/elasticsearch/config/root-ca.pem https://${ELASTICSEARCH_ADMINUSER}:${ELASTICSEARCH_ADMIN_PASSWORD}'
+else
+    ELASTIC_CURL='curl -s -k http://$(hostname)'
+fi
+
 for pod in $(kubectl ${KUBE_OPTS} get pods -l role=elasticsearch | grep -v "NAME" | awk '{print $1}')
 do
-    printf "$pod\t" |tee -a elasticsearch_storage.log
-    kubectl ${KUBE_OPTS} exec -it $pod  -c elasticsearch -- df -Ph | grep elasticsearch | grep -v "tmpfs" | awk '{printf "%-13s %10s %6s %8s %6s %s\n",$1,$2,$3,$4,$5,$6}' |tee -a ${LOG_DIR}/elasticsearch/elasticsearch_storage.log
+    mkdir -p ${LOG_DIR}/elasticsearch/${pod}
+    printf "${pod}\n" | tee -a ${LOG_DIR}/elasticsearch/${pod}/elasticsearch_health.log
+    kubectl ${KUBE_OPTS} exec -it ${pod}  -c elasticsearch -- /bin/bash -c "${ELASTIC_CURL}@sysdigcloud-elasticsearch:9200/_cat/health" | tee -a ${LOG_DIR}/elasticsearch/${pod}/elasticsearch_health.log
+
+    printf "${pod}\n" | tee -a ${LOG_DIR}/elasticsearch/elasticsearch_indices.log
+    kubectl ${KUBE_OPTS} exec -it ${pod}  -c elasticsearch -- /bin/bash -c "${ELASTIC_CURL}@sysdigcloud-elasticsearch:9200/_cat/indices" | tee -a ${LOG_DIR}/elasticsearch//${pod}/elasticsearch_indices.log
+
+    printf "${pod}\n" | tee -a ${LOG_DIR}/elasticsearch/elasticsearch_nodes.log
+    kubectl ${KUBE_OPTS} exec -it ${pod}  -c elasticsearch -- /bin/bash -c "${ELASTIC_CURL}@sysdigcloud-elasticsearch:9200/_cat/nodes?v" | tee -a ${LOG_DIR}/elasticsearch/${pod}/elasticsearch_nodes.log
+
+    printf "${pod}\n" | tee -a ${LOG_DIR}/elasticsearch/elasticsearch_index_allocation.log
+    kubectl ${KUBE_OPTS} exec -it ${pod}  -c elasticsearch -- /bin/bash -c "${ELASTIC_CURL}@sysdigcloud-elasticsearch:9200/_cluster/allocation/explain?pretty" | tee -a ${LOG_DIR}/elasticsearch/${pod}/elasticsearch_index_allocation.log
+
+    printf "${pod}\n" | tee -a ${LOG_DIR}/elasticsearch/${pod}/elasticsearch_storage.log
+    mountpath=$(kubectl ${KUBE_OPTS} get sts sysdigcloud-elasticsearch -ojsonpath='{.spec.template.spec.containers[].volumeMounts[?(@.name == "data")].mountPath}')
+    if [ ! -z $mountpath ]; then
+       echo "Please check this value against the Elasticsearch PV size" | tee -a ${LOG_DIR}/elasticsearch/${pod}/elasticsearch_storage.log
+       kubectl ${KUBE_OPTS} exec -it ${pod} -c elasticsearch -- du -ch ${mountpath} | grep -i total | awk '{printf "%-13s %10s\n",$1,$2}' | tee -a ${LOG_DIR}/elasticsearch/${pod}/elasticsearch_storage.log
+   else
+      printf "Error getting ElasticSearch ${pod} mount path\n" | tee -a ${LOG_DIR}/elasticsearch/${pod}/elasticsearch_storage.log
+   fi
 done
 
 # Fetch Cassandra storage info
-# Executes a df -h in Cassandra pod, gets proxyhistograms, tpstats, and compactionstats
-printf "Pod#\tFilesystem\tSize\tUsed\tAvail\tUse\tMounted on\n" > ${LOG_DIR}/cassandra/cassandra_storage.log
 for pod in $(kubectl ${KUBE_OPTS} get pods -l role=cassandra  | grep -v "NAME" | awk '{print $1}')
 do
-    printf "$pod\t" > ${LOG_DIR}/cassandra/cassandra_storage.log
-    kubectl ${KUBE_OPTS} exec -it $pod -c cassandra -- df -Ph | grep cassandra | grep -v "tmpfs" | awk '{printf "%-13s %10s %6s %8s %6s %s\n",$1,$2,$3,$4,$5,$6}' > ${LOG_DIR}/cassandra/cassandra_storage.log
+    echo "Checking Cassandra Storage - ${pod}"
+    mkdir -p ${LOG_DIR}/cassandra/${pod}
+    printf "${pod}\n" | tee -a ${LOG_DIR}/cassandra/${pod}/cassandra_storage.log
+    mountpath=$(kubectl ${KUBE_OPTS} get sts sysdigcloud-cassandra -ojsonpath='{.spec.template.spec.containers[].volumeMounts[?(@.name == "data")].mountPath}')
+    if [ ! -z $mountpath ]; then
+        echo "Please check this value against the Cassandra PV size" | tee -a ${LOG_DIR}/cassandra/${pod}/cassandra_storage.log
+        kubectl ${KUBE_OPTS} exec -it ${pod} -c cassandra -- du -ch ${mountpath} | grep -i total | awk '{printf "%-13s %10s\n",$1,$2}' | tee -a ${LOG_DIR}/cassandra/${pod}/cassandra_storage.log || true
+   else
+      printf "Error getting Cassandra ${pod} mount path\n" | tee -a ${LOG_DIR}/cassandra/${pod}/cassandra_storage.log
+   fi
+done
+
+# Fetch postgresql storage info
+for pod in $(kubectl ${KUBE_OPTS} get pods -l role=postgresql  | grep -v "NAME" | awk '{print $1}')
+do
+    echo "Checking PostgreSQL Storage - ${pod}"
+    mkdir -p ${LOG_DIR}/postgresql/${pod}
+    printf "${pod}\n" | tee -a ${LOG_DIR}/postgresql/${pod}/postgresql_storage.log
+    echo "Please check this value against the PostgreSQL PV size" | tee -a ${LOG_DIR}/postgresql/${pod}/postgresql_storage.log
+    kubectl ${KUBE_OPTS} exec -it ${pod} -c postgresql -- du -ch /var/lib/postgresql | grep -i total | awk '{printf "%-13s %10s\n",$1,$2}' | tee -a ${LOG_DIR}/postgresql/${pod}/postgresql_storage.log || true
+done
+
+# Fetch mysql storage info
+for pod in $(kubectl ${KUBE_OPTS} get pods -l role=mysql  | grep -v "NAME" | awk '{print $1}')
+do
+    echo "Checking MySQL Storage - ${pod}"
+    mkdir -p ${LOG_DIR}/mysql/${pod}
+    printf "${pod}\n" | tee -a ${LOG_DIR}/mysql/${pod}/mysql_storage.log
+    echo "Please check this value against the mysql PV size" | tee -a ${LOG_DIR}/mysql/${pod}/mysql_storage.log
+    kubectl ${KUBE_OPTS} exec -it ${pod} -c mysql -- du -ch /var/lib/mysql | grep -i total | awk '{printf "%-13s %10s\n",$1,$2}' | tee -a ${LOG_DIR}/mysql/${pod}/mysql_storage.log || true
 done
 
 # Collect the sysdigcloud-config configmap, and write to the log directory
