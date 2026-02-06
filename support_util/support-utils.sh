dc1df59951b22959b8c0c592fe635a56333596b4
ric-sysdig <riccardo.distefano@sysdig.com>
2026-02-06 11:36:32 +0100

    mb review

--- support_util/support-utils.sh
+++ support_util/support-utils.sh
@@ -3,10 +3,16 @@
 namespace=$1
 podName=$2
 
+function log_activity() 
+{
+  local MESSAGE="$1"
+  echo "$(date '+%Y-%m-%d %H:%M:%S') | $MESSAGE" >> "${ACTIVITY_LOG}"
+}
+
 function initVar()
 {
   # Ensure namespace is provided
-  if [ -z "$namespace" ]; then
+  if [[ -z "$namespace" ]]; then
     echo "Error: namespace is a required parameter."
     echo "Usage: $0 namespace [podName - optional]"
     exit 1
@@ -16,13 +22,9 @@ function initVar()
   SYSDIG_SUPPORT_DIR="sysdig-support"
   mkdir -p $DEST_DIR/$SYSDIG_SUPPORT_DIR
 
-  ACTIVITY_LOG="${DEST_DIR}/${SYSDIG_SUPPORT_DIR}/activity.log"
-  function log_activity() {
-    local MESSAGE="$1"
-    echo "$(date '+%Y-%m-%d %H:%M:%S') | $MESSAGE" >> "${ACTIVITY_LOG}"
-  }
+  ACTIVITY_LOG="$DEST_DIR/$SYSDIG_SUPPORT_DIR/activity.log"
   
-  echo "Sysdig Support Script Activity Log - $(date)" > "${ACTIVITY_LOG}"
+  echo "Sysdig Support Script Activity Log - $(date)" > "$ACTIVITY_LOG"
   log_activity "Initialized activity log and created/confirmed output folder."
 
   local CURR_DATE=$(date '+%Y-%m-%d_%H%M%S')
@@ -71,21 +73,19 @@ function initVar()
   SYSDIG_CS_PREFIX=""
   SYSDIG_KSPMC_PREFIX=""
   
-export DEST_DIR
-export ARCHIVE_NAME
-export ERR_RC_TAR
-export ERR_RC_CURL
-export AGENT_LOG_DIR
-export SYSDIG_CS_DIR
-export IS_AIRGAPPED
+  export DEST_DIR
+  export ARCHIVE_NAME
+  export ERR_RC_TAR
+  export ERR_RC_CURL
+  export AGENT_LOG_DIR
+  export SYSDIG_CS_DIR
+  export IS_AIRGAPPED
 
   printf "All the data will be saved using the path %s \n" "$DEST_DIR/$SYSDIG_SUPPORT_DIR"
   log_activity "Data directory set to $DEST_DIR/$SYSDIG_SUPPORT_DIR"
-
   printf "Please put your executable to access your k8s cluster, kubectl or oc\n"
   read k8sCmd
   log_activity "User selected Kubernetes CLI: $k8sCmd"
-
   export k8sCmd
   
   printf "Performing init vars\n"
@@ -156,7 +156,6 @@ done <<< "${SYSDIG_DEPLOY_NAME}"
 
   printf "Init vars completed\n"
   log_activity "initVar completed."
-
   log_activity "Prefix for hostShield/agent $SYSDIG_AGENT_PREFIX"
   log_activity "Prefix for clusterShield $SYSDIG_CS_PREFIX"
   log_activity "Prefix for nodeAnalyzer $SYSDIG_NA_PREFIX"
@@ -177,18 +176,17 @@ function disclaimer()
 
 function isAirgapped()
 {
-
-printf "The script use a curl command to upload the archive file created into your case\n"
-printf "Please put y if your environment has restrictions on internet access or no internet access, or if you just want to generate the archive. \n Put n if your env do not have any kind of restrictions for internet access, this will require the S3 bucket url provided by Sysdig support\n"
-read answer
-log_activity "Selected $answer as response for internet restrictions"
-clear
+  printf "The script use a curl command to upload the archive file created into your case\n"
+  printf "Please put y if your environment has restrictions on internet access or no internet access, or if you just want to generate the archive. \n Put n if your env do not have any kind of restrictions for internet access, this will require the S3 bucket url provided by Sysdig support\n"
+  read answer
+  log_activity "Selected $answer as response for internet restrictions"
+  clear
 }
 
 function collectArtifacts()
 {
   log_activity "Starting collectArtifacts (namespace: ${namespace}, pod: ${podName:-ALL})"
-  if [ -z $podName ]; then
+  if [[ -z $podName ]]; then
     printf "Getting all sysdig pods configuration, this could take a while\n"
     log_activity "Collecting cluster info (version output)."
     $k8sCmd version -o json > $DEST_DIR/$SYSDIG_SUPPORT_DIR/cluster_info.json
@@ -268,7 +266,7 @@ function getLogs()
   log_activity "Starting getLogs (namespace: $namespace, pod: ${podName:-ALL})"
   local csAttempt=0
   local drAgentAttempt=0
-  if [ -z $podName ]; then
+  if [[ -z $podName ]]; then
     printf "Getting all sysdig logs, this could take a while\n"
     for pod in $(awk '{print $1}' < "$DEST_DIR/$SYSDIG_SUPPORT_DIR/running_pods.txt"); do
       printf "Getting log of pod %s \n" "$pod"
@@ -321,7 +319,7 @@ function getLogs()
       if [[ -n "$SYSDIG_KSPMC_PREFIX" ]] && [[ $pod =~ $SYSDIG_KSPMC_PREFIX ]]; then
         mkdir -p $DEST_DIR/$SYSDIG_SUPPORT_DIR/logs/$pod
         $k8sCmd -n $namespace logs $pod > $DEST_DIR/$SYSDIG_SUPPORT_DIR/logs/$pod/$pod.log 2>/dev/null
-        if [ $? -eq 0 ]; then
+        if [[ $? -eq 0 ]]; then
           log_activity "Collected kspmcollector log for pod $pod"
         else
           log_activity "ERROR: Failed to collect kspmcollector log for pod $pod (pod not found)."
@@ -335,7 +333,7 @@ function getLogs()
       printf "Collecting log for pod %s\n" "$podName"
       mkdir -p $DEST_DIR/$SYSDIG_SUPPORT_DIR/logs/$podName
       $k8sCmd -n $namespace cp $podName:${AGENT_LOG_DIR}. $DEST_DIR/$SYSDIG_SUPPORT_DIR/logs/$podName --retries=$AGENT_CP_RETRY 2>/dev/null
-      if [ $? -eq 0 ]; then
+      if [[ $? -eq 0 ]]; then
         printf "Log collected for pod %s\n" "$podName"
         log_activity "Collected agent log for pod $podName "
       else
@@ -347,7 +345,7 @@ function getLogs()
       printf "Collecting log for pod %s\n" "$podName"
       mkdir -p $DEST_DIR/$SYSDIG_SUPPORT_DIR/logs/$SYSDIG_CS_DIR
       $k8sCmd -n $namespace logs $podName > $DEST_DIR/$SYSDIG_SUPPORT_DIR/logs/$SYSDIG_CS_DIR/$podName.log 2>/dev/null
-      if [ $? -eq 0 ]; then
+      if [[ $? -eq 0 ]]; then
         printf "Log collected for pod %s\n" "$podName"
         log_activity "Collected cluster shield log for pod ${podName}."
       else
@@ -375,7 +373,7 @@ function getLogs()
       printf "Collecting log for pod %s\n" "$podName"
       mkdir -p $DEST_DIR/$SYSDIG_SUPPORT_DIR/logs/$podName
       $k8sCmd -n $namespace logs $podName > $DEST_DIR/$SYSDIG_SUPPORT_DIR/logs/$podName/$podName.log 2>/dev/null
-      if [ $? -eq 0 ]; then
+      if [[ $? -eq 0 ]]; then
         printf "Log collected for pod %s\n" "$podName"
         log_activity "Collected kspmcollector log for pod $podName"
       else
@@ -393,7 +391,7 @@ function compressAndUpload()
   printf "Creating archive file %s\n" "$ARCHIVE_NAME"
   log_activity "Starting archive creation: $ARCHIVE_NAME"
   errorMessage=$(tar czf $ARCHIVE_NAME * 2>&1)
-  if [ $? -eq 0 ]; then
+  if [[ $? -eq 0 ]]; then
     printf "Archive file created successfully, ready for upload!\n"
     log_activity "Archive $ARCHIVE_NAME created successfully."
   else
@@ -402,64 +400,57 @@ function compressAndUpload()
     log_activity "Failed to create archive $ARCHIVE_NAME : $errorMessage"
     exit $ERR_RC_TAR
   fi
-if [ $answer == "y" ];
-  then
-  printf "The archive has been saved in the path $DEST_DIR/$SYSDIG_SUPPORT_DIR , archive name is $ARCHIVE_NAME\n"
-  IS_AIRGAPPED="true"
-  cleanArtifacts $IS_AIRGAPPED
-  printf "Script execution completed, exit \n"
-  exit 0
- else
-
-  printf "Please put the S3 link provided by Sysdig Support, the script will upload the archive on the case.\n"
-  read SYSDIG_UPLOAD_URL
-  log_activity "User provided S3 upload URL: $SYSDIG_UPLOAD_URL"
-
-  printf "The status of the upload will not be shown, please be patient\n"
-  log_activity "Uploading $ARCHIVE_NAME to S3 $SYSDIG_UPLOAD_URL"
-  httpRetCode=$(curl -s -S -o /dev/null -w "%{http_code}" -X PUT --url "${SYSDIG_UPLOAD_URL}" -H "Content-Disposition: attachment; filename=${ARCHIVE_NAME}" -T "${ARCHIVE_NAME}")
-  if [ $httpRetCode -eq 200 ]; then
-    printf "Archive file uploaded successfully!\n"
-    log_activity "Archive uploaded successfully to S3."
-    cleanArtifacts
+  if [[ $answer == "y" ]];
+    then
+    printf "The archive has been saved in the path $DEST_DIR/$SYSDIG_SUPPORT_DIR , archive name is $ARCHIVE_NAME\n"
+    IS_AIRGAPPED="true"
+    cleanArtifacts $IS_AIRGAPPED
+    printf "Script execution completed, exit \n"
+    exit 0
   else
-    printf "Something went wrong\n"
-    log_activity "S3 upload failed with curl exit code $httpRetCode"
-    printf "The archive has been saved in the path $DEST_DIR/$SYSDIG_SUPPORT_DIR"
-    exit $ERR_RC_CURL
-  fi
-
+    printf "Please put the S3 link provided by Sysdig Support, the script will upload the archive on the case.\n"
+    read SYSDIG_UPLOAD_URL
+    log_activity "User provided S3 upload URL: $SYSDIG_UPLOAD_URL"
+    printf "The status of the upload will not be shown, please be patient\n"
+    log_activity "Uploading $ARCHIVE_NAME to S3 $SYSDIG_UPLOAD_URL"
+    httpRetCode=$(curl -s -S -o /dev/null -w "%{http_code}" -X PUT --url "${SYSDIG_UPLOAD_URL}" -H "Content-Disposition: attachment; filename=${ARCHIVE_NAME}" -T "${ARCHIVE_NAME}")
+    if [[ $httpRetCode -eq 200 ]]; then
+      printf "Archive file uploaded successfully!\n"
+      log_activity "Archive uploaded successfully to S3."
+      cleanArtifacts
+    else
+      printf "Something went wrong\n"
+      log_activity "S3 upload failed with curl exit code $httpRetCode"
+      printf "The archive has been saved in the path $DEST_DIR/$SYSDIG_SUPPORT_DIR"
+      exit $ERR_RC_CURL
+    fi
   printf "Script execution completed!\n"
   log_activity "Script execution completed."
-fi
+  fi
 }
 
 function cleanArtifacts
 {
-
-printf "Cleanup started\n"
-log_activity "Script Cleanup Started"
-
-cd $DEST_DIR/$SYSDIG_SUPPORT_DIR
-
-printf "Removing cluster_info.json file\n"
-rm -f cluster_info.json
-printf "Removing dragent.yaml file\n"
-rm -f dragent.yaml 
-printf "Removing txt file\n"
-rm -f *.txt
+  printf "Cleanup started\n"
+  log_activity "Script Cleanup Started"
+  cd $DEST_DIR/$SYSDIG_SUPPORT_DIR
+  printf "Removing cluster_info.json file\n"
+  rm -f cluster_info.json
+  printf "Removing dragent.yaml file\n"
+  rm -f dragent.yaml 
+  printf "Removing txt file\n"
+  rm -f *.txt
   
-if [ $IS_AIRGAPPED == "false" ];
-  then
-  printf "Removing archive file $ARCHIVE_NAME \n"
-  rm $ARCHIVE_NAME
-fi
+  if [[ $IS_AIRGAPPED == "false" ]];
+    then
+    printf "Removing archive file $ARCHIVE_NAME \n"
+    rm $ARCHIVE_NAME
+  fi
 
-printf "Removing the logs directory\n"
-rm -fr logs
-
-printf "Cleanup completed!\n"
-log_activity "Script Cleanup completed"
+  printf "Removing the logs directory\n"
+  rm -fr logs
+  printf "Cleanup completed!\n"
+  log_activity "Script Cleanup completed"
 }
 
 # Main workflow
